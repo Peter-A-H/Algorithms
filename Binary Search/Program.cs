53235bce55bf6e89646c8105ba495af3b57d6a93
Peter <peterhamblin@hotmail.co.uk>
2024-02-01 18:10:25 +0000

    Method sorts input data

--- Binary Search/Program.cs
+++ Binary Search/Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿static int BinarySearch(int[] input, int item)
+{
+    Array.Sort(input);
+    int low = 0;
+    int high = input.Length - 1;
+
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        int guess = input[mid];
+
+        if (guess == item) return mid;
+        if (guess > item) high = mid - 1;
+        else low = mid + 1;
+    }
+
+    return -1;
+}
+
+int[] inputArr = [9, 1, 7, 3, 5];
+
+Console.WriteLine(BinarySearch(inputArr, 3)); //1
+Console.WriteLine(BinarySearch(inputArr, 9)); //4
