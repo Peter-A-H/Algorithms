26d9b60542a0a7147a56159fef0cb9eb183999d3
Peter <peterhamblin@hotmail.co.uk>
2024-02-01 20:13:39 +0000

    Added namespaces for code reuse

--- Binary Search/Program.cs
+++ Binary Search/Program.cs
@@ -1,23 +1,6 @@
-﻿static int BinarySearch(int[] input, int itemToSearch)
-{
-    Array.Sort(input);
-    int smallestElement = 0;
-    int largestElement = input.Length - 1;
-
-    while (smallestElement <= largestElement)
-    {
-        int middleElement = (smallestElement + largestElement) / 2;
-        int pickedElement = input[middleElement];
-
-        if (pickedElement == itemToSearch) return middleElement;
-        if (pickedElement > itemToSearch) largestElement = middleElement - 1;
-        else smallestElement = middleElement + 1;
-    }
-
-    return -1;
-}
+﻿using Binary_Search;
 
 int[] inputArr = [9, 1, 7, 3, 5];
 
-Console.WriteLine(BinarySearch(inputArr, 3)); //1
-Console.WriteLine(BinarySearch(inputArr, 9)); //4
+Console.WriteLine(BinarySearch.Search(inputArr, 3)); //1
+Console.WriteLine(BinarySearch.Search(inputArr, 9)); //4
---
+++ Binary Search/Search.cs
@@ -0,0 +1,23 @@
+﻿namespace Binary_Search;
+
+public class BinarySearch
+{
+    public static int Search(int[] input, int itemToSearch)
+    {
+        Array.Sort(input);
+        int smallestElement = 0;
+        int largestElement = input.Length - 1;
+
+        while (smallestElement <= largestElement)
+        {
+            int middleElement = (smallestElement + largestElement) / 2;
+            int pickedElement = input[middleElement];
+
+            if (pickedElement == itemToSearch) return middleElement;
+            if (pickedElement > itemToSearch) largestElement = middleElement - 1;
+            else smallestElement = middleElement + 1;
+        }
+
+        return -1;
+    }
+}
